12fd369376402f50f4ac1ba12aa6407f65da0e7c
prasunmukherjee07 <prasunkmr07@gmail.com>
2024-05-20 14:30:09 +0530

    Created Main Class

--- Calc.java
+++ Calc.java
@@ -1,8 +1,22 @@
+import java.util.*;
+
 interface Calc{
 	int cal(int a, int b);
 }
 class Test implements Calci{
 	public int cal(int a,int b){
 		return a+b;
+	}
+}
+
+class CalcDriver{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the Elements");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		Test t1=new Test();// Upcasting
+		System.out.println(t1.cal(a,b));
+		System.out.println();
 	}
 }
